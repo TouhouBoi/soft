8c5ab7a760f9b97f17afbbe6ec51731845566dab
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-11-21 22:29:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 require("config.php");
 require("repoLib.php");
+
+$xesoft = new XeSoft;
+
+$xesoft->setHeaderType(1);
+$xesoft->genIndex(XESOFT_SERVER_ADDRESS, XESOFT_REPO_NAME, XESOFT_PACKAGES_PATH, XESOFT_XENONOS_VERSION);
