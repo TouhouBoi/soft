8d0d4b8f408c4decbf7a4bbec654cf62e5871e10
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-11-21 22:17:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
 header('Content-Type: text/plain');
-echo '<eyePorts>
-	<portServer>
-		<address>soft.oneye-project.org</address>
-		<name>eyeSoft Repository</name>
-		<path>/packages.xml</path>
-		<version>1.11</version>
-	</portServer>
-</eyePorts>';
+
 ?>
