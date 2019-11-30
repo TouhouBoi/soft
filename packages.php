9bf71406bbaf6e99c6b964e51c17f310cfbdc5bf
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-11-29 23:51:02 -0500

    Update packages.php

--- packages.php
+++ packages.php
@@ -1,3 +1,10 @@
 <?php
 require("config.php");
 require("repoLib.php");
+
+$xesoft = new XeSoft;
+
+$mysql = $xesoft->connectDB(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
+
+$xesoft->setHeaderType(1);
+$xesoft->genPackages(XESOFT_SERVER_ADDRESS, "", $mysql);
